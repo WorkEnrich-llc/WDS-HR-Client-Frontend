71938c4c5d2d3714016e1f0dbea39d06ac81613d
Assem Osama <52135892+AssemOsama@users.noreply.github.com>
2025-04-18 18:21:49 +0200

    update width

--- src/app/components/compo/page-header/page-header.component.ts
+++ src/app/components/compo/page-header/page-header.component.ts
@@ -1,14 +1,21 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, ContentChild, ElementRef, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-page-header',
-  imports: [CommonModule,RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './page-header.component.html',
   styleUrl: './page-header.component.css'
 })
 export class PageHeaderComponent {
   @Input() breadcrumbs: { label: string; link?: string }[] = [];
   @Input() title: string = '';
+  @ContentChild('subinfo', { static: false }) subinfoContent!: ElementRef;
+
+  hasSubinfo: boolean = false;
+
+  ngAfterContentInit() {
+    this.hasSubinfo = this.subinfoContent?.nativeElement?.innerHTML.trim().length > 0;
+  }
 }
