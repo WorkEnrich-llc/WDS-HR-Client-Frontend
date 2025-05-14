3557cb769a69b91186c6b4deb5ce409156adaf1f
Assem Osama <52135892+AssemOsama@users.noreply.github.com>
2025-05-15 00:56:49 +0300

    Org Chart work

--- src/app/components/OD/Organizational-Chart/org-chart/org-chart.component.ts
+++ src/app/components/OD/Organizational-Chart/org-chart/org-chart.component.ts
@@ -1,20 +1,216 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PageHeaderComponent } from '../../../shared/page-header/page-header.component';
-import { OrganizationChartModule } from 'primeng/organizationchart';
+declare var OrgChart: any;
 
 @Component({
   selector: 'app-org-chart',
-  imports: [PageHeaderComponent, CommonModule, OrganizationChartModule],
+  imports: [PageHeaderComponent, CommonModule],
   templateUrl: './org-chart.component.html',
   styleUrls: ['./org-chart.component.css'],
   encapsulation: ViewEncapsulation.None
 })
-export class OrgChartComponent implements OnInit {
-  orgChartData: any;
+export class OrgChartComponent implements OnInit, AfterViewInit {
+  @ViewChild('chartContainer', { static: false }) chartContainer!: ElementRef;
+  root: any;
+  chart: any;
 
+  ngAfterViewInit(): void {
+    OrgChart.templates.cool = Object.assign({}, OrgChart.templates.ana);
+    OrgChart.templates.cool.size = [310, 150];
+    OrgChart.templates.cool.node = `
+    <rect x="0" y="0" height="150" width="310" fill="#ffffff" stroke-width="1" stroke="#000000" rx="10" ry="10"></rect>
+`;
+    OrgChart.templates.cool.field_0 = `<g class="level">
+  
+    <rect x="10" y="10" rx="20" ry="20" width="120" height="30" fill="#f0f0f0" stroke="#333" stroke-width="1" />
+    
+    <text x="20" y="30">{val}</text>
+  </g>`;
+    OrgChart.templates.cool.field_1 = `<text class="code" x="20" y="60">{val}</text>`;
+    OrgChart.templates.cool.field_2 = `<text class="postion" x="20" y="90">{val}</text>`;
+    OrgChart.templates.cool.field_3 = `<text class="name" x="20" y="120">{val}</text>`;
+    OrgChart.templates.cool.svg = `
+            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" 
+                 style="background-color:#F2F2F2;display:block;" width="{w}" height="{h}" viewBox="{viewBox}">
+                {content}
+            </svg>
+        `;
+
+    const nodes: any[] = [];
+    let idCounter = 1;
+
+    const flattenTree = (node: any, parentId: number | null = null) => {
+      const currentId = idCounter++;
+      nodes.push({
+        id: currentId,
+        pid: parentId,
+        points: node.level,
+        name: node.name,
+        title: node.position,
+        title2: [this.padJobTitleCode(node.jobTitleCode)],
+        tags: [this.getNodeTag(node)]
+      });
+
+      if (node.children && node.children.length > 0) {
+        for (const child of node.children) {
+          flattenTree(child, currentId);
+        }
+      }
+    };
+
+    flattenTree(this.root);
+
+    this.chart = new OrgChart(this.chartContainer.nativeElement, {
+      mouseScrool: OrgChart.action.scroll,
+      scaleInitial: OrgChart.match.boundary,
+      enableSearch: false,
+      template: 'cool',
+      nodeBinding: {
+        field_0: 'points',
+        field_1: 'title2',
+        field_2: 'title',
+        field_3: 'name',
+      },
+      classNameField: "class",
+      nodeMouseClick: OrgChart.action.expandCollapseSingle,
+      collapse: {
+        level: 1,
+        allChildren: true
+      },
+      expand: {
+        nodes: [1],
+        allChildren: false
+      },
+
+    });
+
+    this.chart.onNodeClick((_: any, args: any) => {
+      console.log('Node clicked:', args.node.data);
+      this.fitChart();
+      return false;
+    });
+
+    this.chart.on('expand', () => {
+      this.fitChart();
+    });
+
+    this.chart.on('init', () => {
+      this.fitChart();
+    });
+
+    this.chart.load(nodes);
+    this.chart.on('redraw', () => {
+      const levels = document.querySelectorAll('.level');
+      levels.forEach((group: any) => {
+        const text = group.querySelector('text');
+        const rect = group.querySelector('rect');
+
+        if (!text || !rect) return;
+
+        const bbox = text.getBBox();
+        const paddingX = 20;
+
+        rect.setAttribute('x', String(bbox.x - paddingX / 2));
+        rect.setAttribute('y', String(bbox.y - 5));
+        rect.setAttribute('width', String(bbox.width + paddingX));
+        rect.setAttribute('height', String(bbox.height + 10));
+      });
+    });
+
+
+  }
+
+  getNodeTag(node: any): string {
+    if (node.level === 'Executive') return 'executive-tag';
+    if (node.level === 'Senior Management') return 'senior-tag';
+    if (node.level === 'Middle Management') return 'middle-tag';
+    if (node.level === 'Team Leadership') return 'team-leader-tag';
+    return 'default-tag';
+  }
+
+  fitChart(): void {
+    if (this.chart) {
+      const container = this.chartContainer.nativeElement;
+      const containerWidth = container.offsetWidth;
+      const containerHeight = container.offsetHeight;
+      const chartWidth = this.chart.width();
+      const chartHeight = this.chart.height();
+
+      const scaleX = containerWidth / chartWidth;
+      const scaleY = containerHeight / chartHeight;
+      const scale = Math.min(scaleX, scaleY);
+
+      this.chart.setScale(scale);
+      this.chart.center();
+      this.chart.fit();
+    }
+  }
+
+  zoomIn(): void {
+    this.chart.zoom(true);
+  }
+
+  zoomOut(): void {
+    this.chart.zoom(false);
+  }
+
+  openfitChart(): void {
+    if (this.chart) {
+      this.chart.fit();
+      this.chart.expand(0, 'all');
+      const container = this.chartContainer.nativeElement;
+      const containerWidth = container.offsetWidth;
+      const containerHeight = container.offsetHeight;
+      const chartWidth = this.chart.offsetWidth;
+      const chartHeight = this.chart.offsetHeight;
+      const scaleX = containerWidth / chartWidth;
+      const scaleY = containerHeight / chartHeight;
+      const scale = Math.min(scaleX, scaleY);
+      this.chart.zoom(scale);
+      this.chart.fit();
+    }
+  }
+
+  toggleFullScreen() {
+    this.isFullScreen = !this.isFullScreen;
+    const bodyElement = document.querySelector('.body');
+    const sidenav = document.querySelector('.sidenav');
+    const htmlElement = document.documentElement;
+    const btnFixed = document.querySelector('.btn-fixed') as HTMLElement;
+    if (bodyElement) {
+      if (this.isFullScreen) {
+        bodyElement.classList.add('body-full');
+        if (sidenav) {
+          sidenav.classList.add('sidenav-hidden');
+        }
+        htmlElement.style.setProperty('zoom', '1', 'important');
+        if (btnFixed) {
+          btnFixed.style.setProperty('zoom', '0.85', 'important');
+        }
+      } else {
+        bodyElement.classList.remove('body-full');
+        if (sidenav) {
+          sidenav.classList.remove('sidenav-hidden');
+        }
+        htmlElement.style.removeProperty('zoom');
+        if (btnFixed) {
+          btnFixed.style.removeProperty('zoom');
+        }
+      }
+    }
+  }
+  
+  
+  resetTreeToRoot() {
+    location.reload();
+  }
+
+  zoom = 1;
+  isFullScreen = false;
+  @ViewChild('chartBox') chartBox?: ElementRef;
   ngOnInit(): void {
-    const root = {
+    this.root = {
       name: 'Ahmed Youssef',
       position: 'CEO',
       jobTitleCode: 101,
@@ -238,65 +434,8 @@ export class OrgChartComponent implements OnInit {
         }
       ]
     }
-
-    this.orgChartData = [this.convertToOrgChartFormat(root)];
   }
-
-convertToOrgChartFormat(node: any): any {
-  const levelClass = node.level ? node.level.toLowerCase().replace(/\s+/g, '-') : '';
-  return {
-    label: node.name,
-    type: 'person',
-    styleClass: `p-person level-${levelClass}`,
-    expanded: node.expanded ?? false,
-    data: {
-      name: node.name,
-      position: node.position,
-      level: node.level,
-      jobTitleCode: node.jobTitleCode
-    },
-    children: node.children?.map((child: any) => this.convertToOrgChartFormat(child)) || []
-  };
-}
-
-getLevelClass(level: string): string {
-  return 'level-' + (level ? level.toLowerCase().replace(/\s+/g, '-') : '');
-}
-padJobTitleCode(code: string | number): string {
-  return code ? code.toString().padStart(4, '0') : '';
-}
-  toggleCollapse(node: any) {
-    node.collapsed = !node.collapsed;
+  padJobTitleCode(code: string | number): string {
+    return code ? code.toString().padStart(4, '0') : '';
   }
-
-  hasChildren(node: any): boolean {
-    return node.children && node.children.length > 0;
-  }
-
-  zoom = 1;
-  isFullScreen = false;
-
-  zoomIn() {
-    this.zoom += 0.1;
-  }
-
-  zoomOut() {
-    this.zoom = Math.max(0.5, this.zoom - 0.1);
-  }
-
-  toggleFullScreen() {
-    this.isFullScreen = !this.isFullScreen;
-
-    const bodyElement = document.querySelector('.body'); // البحث عن العنصر الذي يحتوي على الكلاس body
-
-    if (bodyElement) {
-      if (this.isFullScreen) {
-        bodyElement.classList.add('body-full');
-      } else {
-        bodyElement.classList.remove('body-full');
-      }
-    }
-  }
-
-
 }
