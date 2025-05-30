0ad90f1b2a3f6d1f131aef658eb5f9c5ce39fa9d
Assem Osama <52135892+AssemOsama@users.noreply.github.com>
2025-05-30 19:12:36 +0300

    create branch some handle

--- src/app/components/OD/Branches/create-new-branch/create-new-branch.component.ts
+++ src/app/components/OD/Branches/create-new-branch/create-new-branch.component.ts
@@ -1,19 +1,25 @@
-import { Component, TemplateRef, ViewChild, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild, ViewEncapsulation } from '@angular/core';
 import { PageHeaderComponent } from '../../../shared/page-header/page-header.component';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { TableComponent } from '../../../shared/table/table.component';
 import { OverlayFilterBoxComponent } from '../../../shared/overlay-filter-box/overlay-filter-box.component';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { PopupComponent } from '../../../shared/popup/popup.component';
+import { Router } from '@angular/router';
+import { BranchesService } from '../../../../core/services/od/branches/branches.service';
+import { ToasterMessageService } from '../../../../core/services/tostermessage/tostermessage.service';
+import { DepartmentsService } from '../../../../core/services/od/departments/departments.service';
+import { debounceTime, distinctUntilChanged, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-create-new-branch',
-  imports: [PageHeaderComponent, CommonModule, TableComponent, OverlayFilterBoxComponent, FormsModule, PopupComponent],
+  imports: [PageHeaderComponent, CommonModule, TableComponent, OverlayFilterBoxComponent, FormsModule, PopupComponent, ReactiveFormsModule],
+  providers: [DatePipe],
   templateUrl: './create-new-branch.component.html',
   styleUrl: './create-new-branch.component.css',
   encapsulation: ViewEncapsulation.None,
 })
-export class CreateNewBranchComponent {
+export class CreateNewBranchComponent implements OnInit {
   //deparment table
   @ViewChild('departmentTableHeader', { static: true }) departmentTableHeader!: TemplateRef<any>;
   @ViewChild('departmentTableRow', { static: true }) departmentTableRow!: TemplateRef<any>;
@@ -26,8 +32,152 @@ export class CreateNewBranchComponent {
   @ViewChild('departmentsOverlay') departmentsOverlay!: OverlayFilterBoxComponent;
   @ViewChild('sectionsOverlay') sectionsOverlay!: OverlayFilterBoxComponent;
 
+
+  todayFormatted: string = '';
+  errMsg: string = '';
+  isLoading: boolean = false;
+
+  constructor(
+    private router: Router,
+    private datePipe: DatePipe,
+    private _BranchesService: BranchesService,
+    private toasterMessageService: ToasterMessageService,
+    private _DepartmentsService: DepartmentsService
+  ) {
+
+
+    const today = new Date();
+    this.todayFormatted = this.datePipe.transform(today, 'dd/MM/yyyy')!;
+    // console.log(this.todayFormatted); 
+  }
+
+
+
+  ngOnInit(): void {
+    // department added and search
+    this.searchSubject.pipe(
+      debounceTime(300),
+      distinctUntilChanged()
+    ).subscribe(search => {
+      this.currentPage = 1;
+      this.getAllDepartment(this.currentPage, search);
+    });
+    this.getAllDepartment(this.currentPage);
+
+
+
+  }
+
+  // form step 1
+  branchStep1: FormGroup = new FormGroup({
+    code: new FormControl(''),
+    name: new FormControl('', [Validators.required]),
+    location: new FormControl(''),
+    maxEmployee: new FormControl('', [Validators.required, Validators.pattern('^[0-9]*$')]),
+  });
+
+
+  // form step 2
+  departments: any[] = [];
+  addeddepartments: any[] = [];
+selectAll: boolean = false;
+  searchTerm: string = '';
+  private searchSubject = new Subject<string>();
+  currentPage: number = 1;
+  totalpages: number = 0;
+  totalItems: number = 0;
+  itemsPerPage: number = 10;
+
+  onSearchChange() {
+    this.searchSubject.next(this.searchTerm);
+  }
+
+  getAllDepartment(
+    pageNumber: number,
+    searchTerm: string = '',
+  ) {
+    this._DepartmentsService.getAllDepartment(pageNumber, this.itemsPerPage, {
+      search: searchTerm || undefined,
+    }).subscribe({
+      next: (response) => {
+        this.currentPage = Number(response.data.page);
+        this.totalItems = response.data.total_items;
+        this.totalpages = response.data.total_pages;
+        this.departments = response.data.list_items.map((item: any) => ({
+          id: item.id,
+          name: item.name,
+
+        }));
+
+      },
+      error: (err) => {
+        console.log(err.error?.details);
+      }
+    });
+  }
+  //checkboxes 
+  toggleSelectAll() {
+    this.departments.forEach(department => {
+      department.selected = this.selectAll;
+    });
+  }
+  toggleDepartment(department: any) {
+    // department.selected = !department.selected;
+    if (!department.selected) {
+      this.selectAll = false;
+    } else if (this.departments.length && this.departments.every(dep => dep.selected)) {
+      this.selectAll = true;
+    }
+  }
+
+  onItemsPerPageChange(newItemsPerPage: number) {
+    this.itemsPerPage = newItemsPerPage;
+    this.currentPage = 1;
+    this.getAllDepartment(this.currentPage);
+  }
+  onPageChange(page: number): void {
+    this.currentPage = page;
+    this.getAllDepartment(this.currentPage);
+  }
+  discardDepartment(): void {
+    this.departmentsOverlay.closeOverlay();
+  }
+  addSelectedDepartments(): void {
+    const selected = this.departments.filter(dep => dep.selected);
+
+    selected.forEach(dep => {
+      const alreadyAdded = this.addeddepartments.some(added => added.id === dep.id);
+      if (!alreadyAdded) {
+        this.addeddepartments.push({
+          id: dep.id,
+          name: dep.name,
+          sectionFrom: 0,
+          sectionTo: 0
+        });
+      }
+    });
+
+    this.departments.forEach(dep => dep.selected = false);
+    this.selectAll = false;
+    this.departmentsOverlay.closeOverlay();
+  }
+
+
+  sortDirection: string = 'asc';
+  currentSortColumn: string = '';
+  sortBy() {
+    this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
+    this.addeddepartments = this.addeddepartments.sort((a, b) => {
+      if (this.sortDirection === 'asc') {
+        return a.id > b.id ? 1 : (a.id < b.id ? -1 : 0);
+      } else {
+        return a.id < b.id ? 1 : (a.id > b.id ? -1 : 0);
+      }
+    });
+  }
+
+  // steps
   currentStep = 1;
-  selectAll: boolean = false;
 
   goNext() {
     if (this.currentStep < 2) {
@@ -41,22 +191,6 @@ export class CreateNewBranchComponent {
     }
   }
 
-  Alldepartments: any[] = [
-    { id: 1, name: 'Human Resources' },
-    { id: 2, name: 'Finance' },
-    { id: 3, name: 'Marketing' },
-    { id: 4, name: 'Sales' },
-    { id: 5, name: 'Information Technology' },
-    { id: 6, name: 'Customer Support' },
-    { id: 7, name: 'Logistics' },
-    { id: 8, name: 'Legal' },
-  ];
-
-
-  departments: any[] = [
-    { id: 3, name: 'Marketing', sectionFrom: 4, sectionTo: 8 },
-    { id: 5, name: 'Information Technology', sectionFrom: 4, sectionTo: 8 },
-  ];
 
 
   sections: any[] = [
@@ -66,36 +200,11 @@ export class CreateNewBranchComponent {
     { id: 7, name: 'Section 7', selected: false },
   ];
 
-  sortDirection: string = 'asc';
-  currentSortColumn: string = '';
-
-  sortBy() {
-    this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
-    this.departments = this.departments.sort((a, b) => {
-      if (this.sortDirection === 'asc') {
-        return a.id > b.id ? 1 : (a.id < b.id ? -1 : 0);
-      } else {
-        return a.id < b.id ? 1 : (a.id > b.id ? -1 : 0);
-      }
-    });
-  }
 
 
 
-  //checkboxes 
-  toggleSelectAll() {
-    this.Alldepartments.forEach(department => {
-      department.selected = this.selectAll;
-    });
-  }
 
-  toggleDepartment(department: any) {
-    if (!department.selected) {
-      this.selectAll = false;
-    } else if (this.Alldepartments.every(dep => dep.selected)) {
-      this.selectAll = true;
-    }
-  }
+
 
 
   // discard popup
@@ -111,7 +220,7 @@ export class CreateNewBranchComponent {
 
   confirmAction() {
     this.isModalOpen = false;
-    // logic to deactivate
+    this.router.navigate(['/branches/all-branches']);
   }
 
 
--- src/app/core/services/od/branches/branches.service.ts
+++ src/app/core/services/od/branches/branches.service.ts
@@ -1,9 +1,34 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AuthHelperService } from '../../authentication/auth-helper.service';
+import { environment } from '../../../../../environments/environment';
+import { Observable, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BranchesService {
 
-  constructor() { }
+  private apiBaseUrl: string;
+    constructor(private _HttpClient: HttpClient, private authHelper: AuthHelperService) {
+      this.apiBaseUrl = environment.apiBaseUrl;
+    }
+  
+    // create a new branch
+    createBranch(branchData: any): Observable<any> {
+      if (!this.authHelper.validateAuth()) {
+        return throwError(() => new Error('Authentication failed'));
+      }
+  
+      const token = this.authHelper.getToken()!;
+      const subdomain = this.authHelper.getSubdomain()!;
+      const url = `${this.apiBaseUrl}od/branches`;
+      const headers = new HttpHeaders().set('Authorization', token).set('SUBDOMAIN', subdomain);
+      return this._HttpClient.post(url, branchData, { headers });
+    }
+
+
+
+
+    
 }
