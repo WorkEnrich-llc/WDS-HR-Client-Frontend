9ccdbe79cae6bd38d8833243a61644a8037e1b75
Assem Osama <52135892+AssemOsama@users.noreply.github.com>
2025-08-06 11:51:59 +0300

    upload file and structure updates

--- src/app/components/system-cloud/system-cloud/system-cloud.component.ts
+++ src/app/components/system-cloud/system-cloud/system-cloud.component.ts
@@ -6,6 +6,13 @@ import { SystemCloudService } from '../../../core/services/system-cloud/system-c
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClient, HttpEvent, HttpEventType, HttpRequest } from '@angular/common/http';
 import { ToastrService } from 'ngx-toastr';
+interface FileItem {
+  id: string;
+  name: string;
+  type: 'File' | 'Folder';
+  parent: string | null;
+  children?: FileItem[];
+}
 
 @Component({
   selector: 'app-system-cloud',
@@ -17,7 +24,9 @@ import { ToastrService } from 'ngx-toastr';
 export class SystemCloudComponent implements OnInit {
   constructor(private _systemCloudService: SystemCloudService, private http: HttpClient, private toasterService: ToastrService
   ) { }
+  dataLoaded: boolean = false;
   loadData: boolean = true;
+  loadFiles: boolean = true;
   isLoading: boolean = false;
   uploadProgress: number = 0;
   newFolderName: string = '';
@@ -25,53 +34,128 @@ export class SystemCloudComponent implements OnInit {
   openedFolderId: string | null = null;
   files: any[] = [];
   allFiles: any[] = [];
+  searchFolderText: string = '';
+  filteredFiles: any[] = [];
   breadcrumb: { id: string | null; name: string }[] = [];
+
   systemTemplates: any[] = [];
+  searchText: string = '';
+  filteredTemplates: any[] = [];
   ngOnInit(): void {
     this.getAllSystemTemplates();
     this.getAllFoldersFiles();
   }
-
-
+  // check all loaded
+  get isAllLoaded(): boolean {
+    return !this.loadData && !this.loadFiles;
+  }
+  // get all system templates 
   getAllSystemTemplates() {
     this.loadData = true;
     this._systemCloudService.getAllTemplates().subscribe({
       next: (response) => {
-        this.systemTemplates = response?.data?.object_info;
-        this.loadData = false;
+        this.systemTemplates = response?.data?.object_info || [];
+        this.filteredTemplates = [...this.systemTemplates];
+
       },
       error: (err) => {
         console.log(err.error?.details);
+
+      },
+      complete: () => {
         this.loadData = false;
       }
     });
   }
 
+  filterTemplates() {
+    const text = this.searchText.toLowerCase();
+    this.filteredTemplates = this.systemTemplates.filter(template =>
+      template.label?.toLowerCase().includes(text)
+    );
+  }
+
+  // get all files and folder
   getAllFoldersFiles(parentId: string | null = null) {
-    this.loadData = true;
+    if (this.dataLoaded) {
+      this.updateCurrentFilesView(parentId);
+      return;
+    }
+
+    this.loadFiles = true;
 
     this._systemCloudService.getFoldersFiles().subscribe({
       next: (response) => {
-        this.allFiles = response?.data?.object_info?.slice().reverse();
+        const objects: FileItem[] = response?.data?.object_info ?? [];
 
-        this.files = this.allFiles.filter(item =>
-          (item.parent === null && parentId === null) ||
-          (item.parent !== null && item.parent === parentId)
-        );
+        this.allFiles = this.flattenFilesRecursively(objects);
+        this.dataLoaded = true;
 
-        if (parentId === null) {
-          this.breadcrumb = [{ id: null, name: 'My Drive' }];
-        }
-
-        this.loadData = false;
+        this.updateCurrentFilesView(parentId);
       },
       error: (err) => {
         console.log(err.error?.details);
-        this.loadData = false;
+      },
+      complete: () => {
+        this.loadFiles = false;
       }
     });
   }
 
+  updateCurrentFilesView(parentId: string | null) {
+    this.files = this.allFiles
+      .filter(item =>
+        (item.parent === null && parentId === null) ||
+        (item.parent !== null && item.parent === parentId)
+      )
+      .reverse();
+
+    this.filteredFiles = [...this.files];
+
+    if (parentId === null) {
+      this.breadcrumb = [{ id: null, name: 'My Drive' }];
+    }
+  }
+
+  filterFolders() {
+    const search = this.searchFolderText.trim().toLowerCase();
+
+    if (!search) {
+      this.filteredFiles = [...this.files];
+      return;
+    }
+
+    this.filteredFiles = this.files.filter(item =>
+      item.name?.toLowerCase().includes(search)
+    );
+  }
+
+  getCleanName(name: string): string {
+  if (!name) return '';
+
+  const withoutExtension = name.replace(/\.[^/.]+$/, '');
+  return withoutExtension.replace(/^\d{8}_\d{6}_/, '');
+}
+
+
+  // tree filter to folders
+  flattenFilesRecursively(items: FileItem[], parentId: string | null = null): FileItem[] {
+    let result: FileItem[] = [];
+
+    items.forEach(item => {
+      const currentItem = { ...item, parent: parentId };
+      result.push(currentItem);
+
+      if (item.children && item.children.length > 0) {
+        const childrenFlattened = this.flattenFilesRecursively(item.children, item.id);
+        result = result.concat(childrenFlattened);
+      }
+    });
+
+    return result;
+  }
+
+  // open folder
   openFolder(folder: { id: string, name: string }) {
     this.openedFolderId = folder.id;
 
@@ -83,15 +167,25 @@ export class SystemCloudComponent implements OnInit {
     this.getAllFoldersFiles(folder.id);
   }
 
-
+  // go to folder from 
   goToFolder(folderId: string | null): void {
     this.openedFolderId = folderId;
-    this.files = this.allFiles.filter(item => item.parent === folderId);
+
+    this.files = this.allFiles
+      .filter(item => item.parent === folderId)
+      .reverse();
+
+    this.filteredFiles = [...this.files];
+
+    if (this.searchFolderText.trim()) {
+      this.filterFolders();
+    }
 
     const index = this.breadcrumb.findIndex(b => b.id === folderId);
     this.breadcrumb = this.breadcrumb.slice(0, index + 1);
   }
 
+  // create new folder
   createFolder() {
     this.isLoading = true;
     this.errMsg = '';
@@ -145,6 +239,7 @@ export class SystemCloudComponent implements OnInit {
     }
   }
 
+  // file upload
   fileUpload(formData: FormData) {
     this.isLoading = true;
     this.errMsg = '';
@@ -163,19 +258,66 @@ export class SystemCloudComponent implements OnInit {
         }
       },
       error: (err: any) => {
-        let errorMessage = 'An error occurred';
-        errorMessage = err.error.data.error_handling[0].error;
         this.isLoading = false;
         this.uploadProgress = 0;
+
+        let errorMessage = 'An error occurred';
+
+        if (
+          err?.error?.data?.error_handling &&
+          Array.isArray(err.error.data.error_handling) &&
+          err.error.data.error_handling.length > 0 &&
+          err.error.data.error_handling[0].error
+        ) {
+          errorMessage = err.error.data.error_handling[0].error;
+        } else if (err?.message) {
+          errorMessage = err.message;
+        }
+
         this.toasterService.error(errorMessage);
       }
+
     });
   }
 
-
+  // get file type
   getFileTypeText(fileType: string): string {
     return (fileType || '').toUpperCase().slice(0, 4);
   }
+  // drag and drop
+  isDragOver: boolean = false;
+
+  onDragOver(event: DragEvent) {
+    event.preventDefault();
+    this.isDragOver = true;
+  }
+
+  onDragLeave(event: DragEvent) {
+    event.preventDefault();
+    this.isDragOver = false;
+  }
+
+  onDrop(event: DragEvent) {
+    event.preventDefault();
+    this.isDragOver = false;
+
+    const files = event.dataTransfer?.files;
+    if (files && files.length > 0) {
+      for (let i = 0; i < files.length; i++) {
+        const file = files[i];
+        const fileNameWithoutExtension = file.name.split('.').slice(0, -1).join('.') || file.name;
+
+        const formData = new FormData();
+        formData.append('name', fileNameWithoutExtension);
+        formData.append('type', 'File');
+        formData.append('parent', this.openedFolderId ?? '');
+        formData.append('file', file);
+
+        this.fileUpload(formData);
+      }
+    }
+  }
+
 
 
   getTextX(fileType: string): number {
@@ -187,7 +329,7 @@ export class SystemCloudComponent implements OnInit {
     }
   }
 
-
+  // format file size
   formatFileSize(bytes: number): string {
     if (bytes === 0) return '0 B';
 
@@ -198,12 +340,14 @@ export class SystemCloudComponent implements OnInit {
     return size.toFixed(2) + ' ' + sizes[i];
   }
 
-
+  // exxpand and collapse file templates
   isExpanded = true;
   toggleCollapse() {
     this.isExpanded = !this.isExpanded;
   }
 
+
+  // change view style grid and list
   selectedView: 'grid' | 'list' = 'grid';
 
   setView(view: 'grid' | 'list') {
@@ -213,7 +357,6 @@ export class SystemCloudComponent implements OnInit {
 
   // create File Popup
   createFilePop = false;
-
   openModalNewFile() {
     this.createFilePop = true;
   }
@@ -221,9 +364,9 @@ export class SystemCloudComponent implements OnInit {
   closeModalFile() {
     this.createFilePop = false;
   }
+
   // create Folder Popup
   createFolderPop = false;
-
   openModalNewFolder() {
     this.createFolderPop = true;
   }
