0638a55c2a6cb140284c2de352bacfd50e9f49c5
waeladelshfq <waeladelshafiq@gmail.com>
2025-08-15 17:15:05 +0300

    get all payroll

--- src/app/components/Payroll/Payroll-components/all-payroll-components/all-payroll-components.component.ts
+++ src/app/components/Payroll/Payroll-components/all-payroll-components/all-payroll-components.component.ts
@@ -2,48 +2,56 @@ import { Component, inject, OnInit, ViewChild, ViewEncapsulation } from '@angula
 import { PageHeaderComponent } from '../../../shared/page-header/page-header.component';
 import { TableComponent } from '../../../shared/table/table.component';
 import { OverlayFilterBoxComponent } from '../../../shared/overlay-filter-box/overlay-filter-box.component';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { debounceTime, filter, Subject, Subscription } from 'rxjs';
-import { ActivatedRoute, RouterLink } from '@angular/router';
+import { FormBuilder, FormGroup, FormsModule } from '@angular/forms';
+import { debounceTime, filter, map, Observable, Subject, Subscription } from 'rxjs';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { ToasterMessageService } from '../../../../core/services/tostermessage/tostermessage.service';
 import { ToastrService } from 'ngx-toastr';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { PayrollComponentsService } from 'app/core/services/payroll/payroll-components/payroll-components.service';
+import { PayrollComponent } from 'app/core/models/payroll';
 
 
 @Component({
   selector: 'app-all-payroll-components',
-  imports: [PageHeaderComponent, TableComponent, OverlayFilterBoxComponent, CommonModule, RouterLink],
+  imports: [PageHeaderComponent, TableComponent, OverlayFilterBoxComponent, CommonModule, RouterLink, FormsModule],
   templateUrl: './all-payroll-components.component.html',
-  styleUrl: './all-payroll-components.component.css'
+  styleUrl: './all-payroll-components.component.css',
+  providers: [DatePipe],
 })
 export class AllPayrollComponentsComponent implements OnInit {
 
   private payrollService = inject(PayrollComponentsService);
+  payrollComponentsList: PayrollComponent[] = [];
+  payrollComponents: PayrollComponent[] = [];
+  filteredList: PayrollComponent[] = [];
   constructor(
     private route: ActivatedRoute,
     private toasterMessageService: ToasterMessageService,
     private toastr: ToastrService,
-    private fb: FormBuilder) { }
+    private fb: FormBuilder,
+    private datePipe: DatePipe,
+    private router: Router
+  ) { }
 
 
   @ViewChild('filterBox') filterBox!: OverlayFilterBoxComponent;
-  payrollComponents = [
-    {
-      id: 1,
-      componentName: 'Bonus',
-      type: 'Fixed',
-      classfication: 'Earning',
-      show: true
-    },
-    {
-      id: 2,
-      componentName: 'Absence',
-      type: 'Variable',
-      classfication: 'Deduction',
-      show: false
-    },
-  ];
+  // payrollComponents = [
+  //   {
+  //     id: 1,
+  //     componentName: 'Bonus',
+  //     type: 'Fixed',
+  //     classfication: 'Earning',
+  //     show: true
+  //   },
+  //   {
+  //     id: 2,
+  //     componentName: 'Absence',
+  //     type: 'Variable',
+  //     classfication: 'Deduction',
+  //     show: false
+  //   },
+  // ];
   loadData: boolean = false;
   filterForm!: FormGroup;
   searchTerm: string = '';
@@ -52,17 +60,23 @@ export class AllPayrollComponentsComponent implements OnInit {
   totalItems: number = 0;
   currentPage: number = 1;
   itemsPerPage: number = 10;
+  data: any[] = [];
+
+
+  totalPages: number = 0;
+  components: any[] = [];
+
+
+
+
   private searchSubject = new Subject<string>();
   private toasterSubscription!: Subscription;
 
 
   ngOnInit(): void {
-    this.payrollService.getAllComponents().subscribe((data) => {
-      console.log('Payroll Components:', data);
-    });
     this.route.queryParams.subscribe(params => {
-      // this.currentPage = +params['page'] || 1;
-      // this.getAllDepartment(this.currentPage);
+      this.currentPage = +params['page'] || 1;
+      this.getAllComponents(this.currentPage);
     });
 
     this.toasterSubscription = this.toasterMessageService.currentMessage$
@@ -75,47 +89,116 @@ export class AllPayrollComponentsComponent implements OnInit {
       });
 
     this.searchSubject.pipe(debounceTime(300)).subscribe(value => {
-      // this.getAllDepartment(this.currentPage, value);
+      this.getAllComponents(this.currentPage, value);
     });
 
+  }
+
+  getAllComponents(
+    pageNumber: number,
+    searchTerm: string = '',
+    filters?: {
+      status?: string;
+      updated_from?: string;
+      updated_to?: string;
+      created_from?: string;
+      created_to?: string;
+    }
+  ) {
+    this.loadData = true;
+    this.payrollService.getAllComponent(pageNumber, this.itemsPerPage, {
+      search: searchTerm || undefined,
+      ...filters
+    }).subscribe({
+      next: (response) => {
+        this.currentPage = Number(response.data.page);
+        this.totalItems = response.data.total_items;
+        this.totalPages = response.data.total_pages;
+        this.components = response.data.list_items.map((item: any) => ({
+          id: item.id,
+          name: item.name,
+          classification: item.classification.name,
+          component_type: item.component_type.name,
+          show_in_payslip: item.show_in_payslip,
+          show_in_payslip_label: item.show_in_payslip ? 'Shown' : 'Hidden'
+        }));
+        this.sortDirection = 'desc';
+        this.currentSortColumn = 'id';
+        this.sortBy();
+        this.loadData = false;
+      },
+      error: (err) => {
+        console.log(err.error?.details);
+        this.loadData = false;
+      }
+    });
   }
 
   sortBy() {
     this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
-    this.payrollComponents = this.payrollComponents.sort((a, b) => {
-      const nameA = a.componentName.toLowerCase();
-      const nameB = b.componentName.toLowerCase();
-
+    this.components.sort((a, b) => {
+      const nameA = a.name?.toLowerCase() || '';
+      const nameB = b.name?.toLowerCase() || '';
       if (this.sortDirection === 'asc') {
-        return nameA > nameB ? 1 : (nameA < nameB ? -1 : 0);
+        return nameA.localeCompare(nameB);
       } else {
-        return nameA < nameB ? 1 : (nameA > nameB ? -1 : 0);
+        return nameB.localeCompare(nameA);
       }
     });
   }
-  resetFilterForm(): void {
 
-    this.filterBox.closeOverlay();
-    // this.getAllDepartment(this.currentPage);
+  applyFilters() {
+    this.filteredList = this.components.filter(item =>
+      item.name?.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
+    this.sortBy();
   }
+
+
+  resetFilterForm(): void {
+    this.filterBox.closeOverlay();
+    this.searchTerm = '';
+    this.filteredList = [...this.components];
+  }
+
+
   onSearchChange() {
     this.searchSubject.next(this.searchTerm);
   }
+
   onItemsPerPageChange(newItemsPerPage: number) {
     this.itemsPerPage = newItemsPerPage;
     this.currentPage = 1;
-    // this.getAllDepartment(this.currentPage);
+    this.getAllComponents(this.currentPage);
   }
   onPageChange(page: number): void {
     this.currentPage = page;
-    // this.getAllDepartment(this.currentPage);
+    this.getAllComponents(this.currentPage);
   }
 }
 
 
 
+// resetFilterForm(): void {
+
+//   this.filterBox.closeOverlay();
+//   // this.getAllDepartment(this.currentPage);
+// }
 
 
 
 
-
+// getPayrollComponents() {
+//   this.loadData = true;
+//   this.payrollService.getAllComponents().subscribe({
+//     next: (res: any) => {
+//       this.data = res?.data?.list_items || [];
+//       this.totalItems = res?.data?.total || 0;
+//       this.loadData = false;
+//     },
+//     error: (err) => {
+//       console.error(err);
+//       this.loadData = false;
+//     }
+//   });
+// }
--- src/app/core/services/payroll/payroll-components/payroll-components.service.ts
+++ src/app/core/services/payroll/payroll-components/payroll-components.service.ts
@@ -1,8 +1,8 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { PayrollComponent } from 'app/core/models/payroll';
 import { environment } from 'environments/environment';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -21,10 +21,6 @@ export class PayrollComponentsService {
     return this.http.post<PayrollComponent>(this.url, data);
   }
 
-  // createComponent(componentData: any): Observable<any> {
-  //   return this.http.post<any>(this.url, componentData);
-  // }
-
   // update component
   updateComponent(id: number, componentData: PayrollComponent): Observable<PayrollComponent> {
     const data = { request_data: componentData };
@@ -37,9 +33,50 @@ export class PayrollComponentsService {
   }
 
   // Get payroll component by ID 
-  getComponentById(id: number): Observable<PayrollComponent> {
-    return this.http.get<PayrollComponent>(`${this.url}/${id}`);
+  // getComponentById(id: number): Observable<PayrollComponent> {
+  //   return this.http.get<PayrollComponent>(`${this.url}/${id}`);
+  // }
+
+  getComponentById(id: number) {
+    return this.http.get<PayrollComponent>(`${this.url}/${id}`).pipe(
+      map((res: any) => res.data?.object_info ?? null)
+    );
   }
+
+  getAllComponent(
+    pageNumber: number,
+    perPage: number,
+    filters?: {
+      search?: string;
+      updated_from?: string;
+      updated_to?: string;
+      created_from?: string;
+      created_to?: string;
+      show_in_payslip?: string;
+      branch_id?: number;
+    }
+  ): Observable<any> {
+    // const url = `${this.apiBaseUrl}od/departments`;
+
+    let params = new HttpParams()
+      .set('page', pageNumber)
+      .set('per_page', perPage);
+
+    if (filters) {
+      if (filters.search) params = params.set('search', filters.search);
+      if (filters.updated_from) params = params.set('updated_from', filters.updated_from);
+      if (filters.updated_to) params = params.set('updated_to', filters.updated_to);
+      if (filters.created_from) params = params.set('created_from', filters.created_from);
+      if (filters.created_to) params = params.set('created_to', filters.created_to);
+      if (filters.show_in_payslip) params = params.set('show_in_payslip', filters.show_in_payslip);
+      if (filters.branch_id != null) params = params.set('branch_id', filters.branch_id.toString());
+    }
+
+    return this.http.get(this.url, { params });
+  }
+
+
+
 
 }
 
