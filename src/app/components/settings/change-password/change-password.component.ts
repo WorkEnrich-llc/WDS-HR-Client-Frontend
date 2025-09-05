d150190ee8e89ac9d370bd1fb589e5486ef0ca04
waeladelshfq <waeladelshafiq@gmail.com>
2025-09-05 04:50:17 +0300

    add change password

---
+++ src/app/components/settings/change-password/change-password.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { InputPasswordComponent } from 'app/components/shared/input-password/input-password.component';
+
+@Component({
+  selector: 'app-change-password',
+  imports: [InputPasswordComponent],
+  templateUrl: './change-password.component.html',
+  styleUrl: './change-password.component.css'
+})
+export class ChangePasswordComponent {
+
+}
