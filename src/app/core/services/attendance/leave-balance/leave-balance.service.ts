c0e26f9f1f3930ba4e2ab1d41eb9d9413a08162d
waeladelshfq <waeladelshafiq@gmail.com>
2025-09-30 08:45:21 +0300

    update leave balance

--- src/app/core/services/attendance/leave-balance/leave-balance.service.ts
+++ src/app/core/services/attendance/leave-balance/leave-balance.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { ILeaveBalanceFilters, ILeaveBalanceResponse } from 'app/core/models/leave-balance';
+import { ILeaveBalanceFilters, ILeaveBalanceResponse, IUpdateLeaveBalance } from 'app/core/models/leave-balance';
 import { environment } from 'environments/environment';
 import { Observable } from 'rxjs';
 
@@ -12,7 +12,6 @@ export class LeaveBalanceService {
   http = inject(HttpClient);
   private readonly baseUrl = `${environment.apiBaseUrl}personnel/employees/leave-balance`;
 
-
   getAllLeaveBalance(params: ILeaveBalanceFilters = {}): Observable<ILeaveBalanceResponse> {
     let httpParams = new HttpParams();
     httpParams = httpParams.set('page', String(params.page ?? 1));
@@ -22,30 +21,18 @@ export class LeaveBalanceService {
         httpParams = httpParams.set(key, String(value));
       }
     });
+
     return this.http.get<ILeaveBalanceResponse>(this.baseUrl, { params: httpParams });
   }
 
+  updateLeaveBalance(data: IUpdateLeaveBalance): Observable<any> {
+    const url = this.baseUrl;
+    const body = {
+      request_data: data
+    };
+    return this.http.put<any>(url, body);
+  }
 
-  //Get Leave Balance
-  // getAllLeaveBalances(
-  //   page: number,
-  //   perPage: number,
-  //   filters?: ILeaveBalanceFilters
-  // ): Observable<ILeaveBalanceResponse> {
-  //   let params = new HttpParams()
-  //     .set('page', page)
-  //     .set('per_page', perPage);
-
-  //   if (filters) {
-  //     Object.entries(filters).forEach(([key, value]) => {
-  //       if (value !== undefined && value !== null && value !== '') {
-  //         params = params.set(key, String(value));
-  //       }
-  //     });
-  //   }
-
-  //   return this.http.get<ILeaveBalanceResponse>(this.baseUrl, { params });
-  // }
 }
 
 
