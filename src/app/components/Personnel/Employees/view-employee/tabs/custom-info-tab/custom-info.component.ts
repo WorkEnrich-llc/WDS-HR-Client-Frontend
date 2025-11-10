f542419a3fe395e2d68bb68be51e5d7a2285585c
waeladelshfq <waeladelshafiq@gmail.com>
2025-11-10 20:31:55 +0200

    custom field values

--- src/app/components/Personnel/Employees/view-employee/tabs/custom-info-tab/custom-info.component.ts
+++ src/app/components/Personnel/Employees/view-employee/tabs/custom-info-tab/custom-info.component.ts
@@ -1,7 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, EventEmitter, inject, Input, OnInit, Output } from '@angular/core';
+import { FormArray, FormControl, FormGroup, FormsModule, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { PopupComponent } from 'app/components/shared/popup/popup.component';
 import { Employee } from 'app/core/interfaces/employee';
+import { CustomFieldObject, CustomFieldValueItem, CustomFieldValuesParams, UpdateCustomValueRequest, UpdateFieldRequest } from 'app/core/models/custom-field';
+import { CustomFieldsService } from 'app/core/services/personnel/custom-fields/custom-fields.service';
+import { ToasterMessageService } from 'app/core/services/tostermessage/tostermessage.service';
 
 interface CustomField {
   label: string;
@@ -15,61 +20,195 @@ interface EditState {
 
 @Component({
   selector: 'app-custom-info',
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, PopupComponent],
   templateUrl: './custom-info.component.html',
   styleUrl: './custom-info.component.css'
 })
 export class CustomInfoComponent implements OnInit {
   @Input() employee: Employee | null = null;
   @Output() fieldsUpdated = new EventEmitter<CustomField[]>();
+  private route = inject(ActivatedRoute);
+  private customFieldsService = inject(CustomFieldsService);
+  private toasterService = inject(ToasterMessageService);
+  customFieldValues: CustomFieldValueItem[] = [];
   editStates: EditState[] = [];
+  isDeleteModalOpen: boolean = false;
+  selectedField!: number;
+  isLoading = false
+  readonly app_name = 'personnel';
 
-  fields: CustomField[] = [
-    { label: 'License ID', value: '12345-ABC' },
-    { label: 'License Expiry Date', value: '24/5/2026' },
-    { label: 'Date of Birth', value: '1/8/1992' }
-  ];
+  customInfoForm: FormGroup = new FormGroup({
+    fields: new FormArray([])
+  });
+
+  get fieldsArray() {
+    return this.customInfoForm.get('fields') as FormArray;
+  }
 
   ngOnInit(): void {
-    this.resetEditStates();
+    const modelName = this.route.snapshot.parent?.data['model_name'];
+    console.log('Model Name:', modelName);
+
+    const objectId = this.route.snapshot.paramMap.get('id');
+    console.log('Object ID:', objectId);
+    this.loadCustomValues();
+
   }
 
-  resetEditStates(): void {
-    this.editStates = this.fields.map(field => ({
-      isEditing: false,
-      editValue: field.value
-    }));
-  }
 
+  createFieldGroup(item: CustomFieldValueItem): FormGroup {
+    const fieldOptions = item.custom_field.input_option;
 
-  onEdit(i: number) {
-    this.editStates[i].isEditing = true;
-    setTimeout(() => {
-      const input = document.querySelectorAll('.edit-input')[i] as HTMLInputElement;
-      input?.focus();
+    return new FormGroup({
+      value_id: new FormControl(item.id),
+      custom_field: new FormControl(item.custom_field),
+      originalValue: new FormControl(item.value.value),
+      label: new FormControl({ value: fieldOptions.label, disabled: true }),
+      value: new FormControl(
+        { value: item.value.value, disabled: true },
+        [
+          fieldOptions.required ? Validators.required : null,
+          fieldOptions.min_length ? Validators.minLength(fieldOptions.min_length) : null,
+          fieldOptions.max_length ? Validators.maxLength(fieldOptions.max_length) : null,
+        ].filter(v => v !== null) as ValidatorFn[]
+      )
     });
   }
 
+  onEdit(index: number): void {
+    this.fieldsArray.at(index)?.get('value')?.enable();
+  }
 
-  onRemove(index: number): void {
-    this.fields.splice(index, 1);
-    this.editStates.splice(index, 1);
+  onCancel(index: number): void {
+    const group = this.fieldsArray.at(index) as FormGroup;
+    const control = group.get('value');
+    const originalValue = group.get('originalValue')?.value;
 
-    this.fieldsUpdated.emit(this.fields);
+    if (control) {
+      control.setValue(originalValue);
+      control.disable();
+    }
   }
 
 
   onConfirm(index: number): void {
-    this.fields[index].value = this.editStates[index].editValue;
-    this.editStates[index].isEditing = false;
-
-    this.fieldsUpdated.emit(this.fields);
-
+    const group = this.fieldsArray.at(index) as FormGroup;
+    const control = group.get('value');
+    if (!group || !control) return;
+    if (group.invalid) {
+      group.markAllAsTouched();
+      return;
+    }
+    control.disable();
+    const rawData = group.getRawValue();
+    const payload: UpdateCustomValueRequest = {
+      request_data: {
+        id: rawData.value_id,
+        value: rawData.value
+      }
+    };
+    this.isLoading = true;
+    this.customFieldsService.updateCustomFieldValue(payload).subscribe({
+      next: () => {
+        this.isLoading = false;
+        this.toasterService.showSuccess('Updated successfully');
+        group.get('originalValue')?.setValue(rawData.value);
+      },
+      error: (err) => {
+        this.isLoading = false;
+        control.enable();
+        this.toasterService.showError('Error updating value');
+        console.error('Update Failed:', err);
+      }
+    });
   }
 
 
-  onCancel(index: number): void {
-    this.editStates[index].isEditing = false;
+
+  onRemove(index: number): void {
+    const group = this.fieldsArray.at(index) as FormGroup;
+    const customField = group.get('custom_field')?.value as CustomFieldObject;
+    if (!customField) {
+      console.error(" Custom field data not found.");
+      return;
+    }
+    const payload: UpdateFieldRequest = {
+      request_data: {
+        id: customField.id,
+        target_model: customField.target_model,
+        input_option: customField.input_option
+      }
+    };
+
+    this.isLoading = true;
+    this.customFieldsService.deleteCustomFieldValue(payload).subscribe({
+      next: () => {
+        this.isLoading = false;
+        this.closeDeleteModal();
+        this.toasterService.showSuccess('Deleted successfully');
+        this.fieldsArray.removeAt(index);
+      },
+      error: (err) => {
+        this.isLoading = false;
+        this.closeDeleteModal();
+
+        console.error('Delete Failed:', err);
+      }
+    });
   }
+
+
+
+  loadCustomValues(): void {
+    this.isLoading = true;
+
+    // const modelName = this.route.snapshot.parent?.data['model_name'];
+    const modelName = 'employees';
+    console.log('Model Name:', modelName);
+
+    const objectId = this.route.snapshot.paramMap.get('id');
+    console.log('Object ID:', objectId);
+
+    if (!modelName || !objectId) {
+      console.error('Could not load custom field values');
+      this.isLoading = false;
+      return;
+    }
+
+    const params: CustomFieldValuesParams = {
+      app_name: this.app_name,
+      model_name: modelName,
+      object_id: objectId
+    };
+
+
+    this.customFieldsService.getCustomFieldValues(params).subscribe({
+      next: (response) => {
+        this.customFieldValues = response.data.list_items;
+        console.log('Custom Field Values:', this.customFieldValues);
+
+        this.fieldsArray.clear();
+        this.customFieldValues.forEach(item => {
+          this.fieldsArray.push(this.createFieldGroup(item));
+        });
+        this.isLoading = false;
+      },
+      error: (err) => {
+        this.isLoading = false;
+        console.error(' Error get data:', err);
+      }
+    });
+  }
+
+  openDeleteModal(index: number): void {
+    this.selectedField = index;
+    this.isDeleteModalOpen = true;
+  }
+
+  closeDeleteModal(): void {
+    this.isDeleteModalOpen = false;
+    this.selectedField = null!;
+  }
+
 
 }
