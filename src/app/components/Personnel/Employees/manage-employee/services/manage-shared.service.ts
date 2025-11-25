4a400b85c9d07a65719e801614b2ea90cad6dee9
mohamedabdelhady131 <mohamedabdelhady131@gmail.com>
2025-11-25 03:40:20 +0200

    show images in create employee

--- src/app/components/Personnel/Employees/manage-employee/services/manage-shared.service.ts
+++ src/app/components/Personnel/Employees/manage-employee/services/manage-shared.service.ts
@@ -122,7 +122,7 @@ export class ManageEmployeeSharedService {
   private initializeForm(): void {
     this.employeeForm = this.fb.group({
       main_information: this.fb.group({
-        profile_image:new FormControl(''),
+        profile_image: new FormControl(''),
         code: [''],
         name_english: ['', [Validators.required, fourPartsValidator()]],
         name_arabic: ['', [Validators.required, arabicNameValidator()]],
@@ -1408,6 +1408,7 @@ export class ManageEmployeeSharedService {
     const requestData: any = {
       main_information: {
         code: formData.main_information.code,
+        profile_image: formData.main_information.profile_image,
         name_english: formData.main_information.name_english,
         name_arabic: formData.main_information.name_arabic,
         gender: formData.main_information.gender,
